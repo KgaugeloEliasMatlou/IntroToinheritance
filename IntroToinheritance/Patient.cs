9b4817ef2d7b7a17ffaa68facef26d47bb0c568d
Train 08 <Train 08@DESKTOP-0JKGM9D>
2022-06-22 09:39:14 +0200

    Add project files.

---
+++ IntroToinheritance/Patient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+public class Patient //First Class
+{
+    //properties
+
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public  int age { get; set; }
+    public int Weight { get; set; }
+    public long SSN { get; set; }
+
+    //Methods
+
+    public void Examine(string pname)
+    {
+        Console.WriteLine("Examination of "+pname+"  completed");
+    }
+
+    public void Billing(long ssn)
+    {
+        Console.WriteLine("Billing completed....");
+    }
+
+}
+public class Child : Patient // second Class
+{
+    public void Inoculate()
+    {
+        Console.WriteLine("Child has been inoculated..");
+    }
+}
+
+public class Adult:Patient
+{
+    public void Adultmessage()
+    {
+        Console.WriteLine("This is an Adult class...");
+    }
+}
+
---
+++ IntroToinheritance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Patient p = new Patient();
+        p.Examine("Jamison");
+
+        Child c = new Child();
+        c.Inoculate();
+        c.Examine("Junior Jamison");
+        
+        Adult a = new Adult();
+
+        a.Adultmessage();
+
+
+        Console.ReadKey();
+    }
+}
